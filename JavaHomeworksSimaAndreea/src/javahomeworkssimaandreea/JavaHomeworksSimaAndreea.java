007efce6bcf388cdad3e107bb76e9c3977923390
Andreea Sima <andreea.202@gmail.com>
2016-11-11 22:32:15 +0200

    Update JavaHomeworksSimaAndreea.java

--- JavaHomeworksSimaAndreea/src/javahomeworkssimaandreea/JavaHomeworksSimaAndreea.java
+++ JavaHomeworksSimaAndreea/src/javahomeworkssimaandreea/JavaHomeworksSimaAndreea.java
@@ -18,4 +18,41 @@ public class JavaHomeworksSimaAndreea {
         // TODO code application logic here
     }
     
-}
+}/** Exercitiul 2
+ *
+ * @author Deea
+ */
+public class JavaHomeworksAndreeaSima {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String yourName = "Andreea";
+       yourName = ("Andreea");
+       int yourAge = 29;
+
+     
+      System.out.println("Hello"+ yourName);
+      System.out.println("hello, your age is" + yourAge);
+        
+        
+        package wrapper;
+
+/** Exercitiul 3
+ *
+ * @author Deea
+ */
+public class Wrapper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    Float f = 15.6F;  // Float wrapper object with value 15.6
+    Long l = new Long("526");  // Long wrapper object with value 526
+    Character c = '@';  // Character object with value @
+    Boolean b = Boolean.valueOf("TRUE");   // Boolean object with value true
+    System.out.printf(" Result: f=%.2f, l=%d, c=%c, b=%b", f, l, c, b );  
+
