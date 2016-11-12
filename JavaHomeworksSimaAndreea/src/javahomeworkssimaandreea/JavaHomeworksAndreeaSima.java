b22d65f8ba112233f6861e0515538b9db39e5599
Deea <Deea@192.168.1.10>
2016-11-12 10:25:59 +0200

    Homework 1 done

---
+++ JavaHomeworksSimaAndreea/src/javahomeworkssimaandreea/JavaHomeworksAndreeaSima.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javahomeworkssimaandreea;
+
+/** Exercitiul 2
+ *
+ * @author Deea
+ */
+public class JavaHomeworksAndreeaSima {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String yourName = "Andreea";
+       yourName = ("Andreea");
+       int yourAge = 29;
+
+     
+      System.out.println("Hello"+ yourName);
+      System.out.println("hello, your age is" + yourAge);
+    }
+}
+        
+        
+      
--- JavaHomeworksSimaAndreea/src/wrapper/Wrapper.java
+++ JavaHomeworksSimaAndreea/src/wrapper/Wrapper.java
@@ -1,8 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+   package wrapper;
 
- 
-package wrapper;
-
-/**
+/** Exercitiul 3
  *
  * @author Deea
  */
@@ -18,8 +21,7 @@ public class Wrapper {
     Character c = '@';  // Character object with value @
     Boolean b = Boolean.valueOf("TRUE");   // Boolean object with value true
     System.out.printf(" Result: f=%.2f, l=%d, c=%c, b=%b", f, l, c, b );  
-
-            
     }
-    
 }
+
+
