5ba8b2b457683bcd0b839fbd0b36b406b0ceaf09
Deea <Deea@192.168.1.10>
2016-11-29 23:14:38 +0200

    Anonymous classes replaced with normal classes

---
+++ JavaHomeworksSimaAndreea/src/Tema5/HelloWorldWithoutAnonymousClasses.java
@@ -0,0 +1,144 @@
+/*
+ * Implements the HelloWorldAnonymousClasses class without using any 
+   anonymous classes
+ */
+package Tema5;
+
+/**
+ *
+ * @author Deea
+ */
+public class HelloWorldWithoutAnonymousClasses {
+    
+    public static void main(String[] args) {
+
+        HelloWorldWithoutAnonymousClasses myApp =
+
+            new HelloWorldWithoutAnonymousClasses();
+
+        myApp.sayHello();
+
+    }           
+
+    
+    
+      //HelloWorld interface
+
+    interface HelloWorld {
+
+
+        public void greet();
+
+
+        public void greetSomeone(String someone);
+
+
+    }
+
+   
+
+    public void sayHello() {
+
+
+        // Local class EnglishGreeting implements HelloWorld
+        
+        class EnglishGreeting implements HelloWorld {
+
+            String name = "world";
+
+
+            @Override
+            public void greet() {
+
+                greetSomeone("world");
+
+            }
+
+
+            @Override
+            public void greetSomeone(String someone) {
+
+                name = someone;
+
+                System.out.println("Hello " + name);
+
+            }
+
+        }         
+        
+        
+        // Local class FrenchGreeting implements HelloWorld
+        
+        class FrenchGreeting implements HelloWorld {
+
+            String name = "tout le monde";
+            
+
+            @Override
+            public void greet() {
+
+                greetSomeone("tout le monde");
+
+            }
+
+
+            @Override
+            public void greetSomeone(String someone) {
+
+                name = someone;
+
+                System.out.println("Salut " + name);
+
+            }
+            
+        }
+        
+        
+        // Local class SpanishGreeting implements HelloWorld
+
+        class SpanishGreeting implements HelloWorld {
+
+            String name = "mundo";
+            
+
+            @Override
+            public void greet() {
+
+                greetSomeone("mundo");
+
+            }
+
+            @Override
+            public void greetSomeone(String someone) {
+
+                name = someone;
+
+                System.out.println("Hola, " + name);
+
+            } 
+
+        }
+        
+        /* The englishGreeting, frenchGreeting and spanishGreeting variables 
+           are instances of the EnglishGreeting, FrenchGreeting and
+           SpanishGreeting classes. 
+           As their type is HelloWorld they can only access 
+           methods overriden from the HelloWorld interface.
+        */
+        
+        HelloWorld englishGreeting = new EnglishGreeting();
+        HelloWorld frenchGreeting = new FrenchGreeting();
+        HelloWorld spanishGreeting =  new SpanishGreeting();
+        
+        
+        englishGreeting.greet();
+
+        frenchGreeting.greetSomeone("Fred");
+
+        spanishGreeting.greet();
+
+    }        
+
+}
+    
+
